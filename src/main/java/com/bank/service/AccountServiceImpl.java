fdd81b5e05ff6d85f2006905b48de01beddb595f
alexsrtoff <alexei.strelnikov@gmail.com>
2020-10-08 12:39:28 +0300

    Service interfaces + logger

--- src/main/java/com/bank/service/AccountServiceImpl.java
+++ src/main/java/com/bank/service/AccountServiceImpl.java
@@ -7,10 +7,12 @@ import com.bank.repository.ClientRepository;
 import com.bank.repository.ClientRepositoryImpl;
 import com.bank.repository.txManager.TxManager;
 import com.bank.repository.txManager.TxManagerImpl;
+import lombok.extern.slf4j.Slf4j;
 
 import java.sql.SQLException;
 import java.util.List;
 
+@Slf4j
 public class AccountServiceImpl implements AccountInterface {
 
     private final AccountRepository accountRepository;
@@ -26,35 +28,43 @@ public class AccountServiceImpl implements AccountInterface {
 
     @Override
     public List<Account> getAll(int id) {
+        log.trace("id={}", id);
         List<Account> allClientAccounts = null;
         try {
             allClientAccounts = accountRepository.getAllClientAccounts(id);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            log.warn("id={}", id, e);
         }
+        log.trace("returning={}", allClientAccounts);
+
         return allClientAccounts;
     }
 
     @Override
     public Account getById(int id) {
+        log.trace("id={}", id);
         Account account = null;
         try {
             account = accountRepository.getAccountById(id);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            log.warn("id={}", id, e);
         }
+        log.trace("returning={}", account);
         return account;
     }
 
 
     @Override
     public Account add(int clientId, Account account) {
+        log.trace("got={}", account);
         Account account1 = new Account();
         try {
             account1 = accountRepository.addAccount(clientId, account);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            log.warn("Account was not created!", e);
         }
+        log.trace("returning={}", account1);
+
         return account1;
     }
 
@@ -65,24 +75,30 @@ public class AccountServiceImpl implements AccountInterface {
 
     @Override
     public Account update(Account account) {
+        log.trace("got={}", account);
 
         try {
             accountRepository.updateAccount(account);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            log.warn("Account was not updated!", e);
         }
+        log.trace("returning={}", account);
         return account;
     }
 
     @Override
     public boolean delete(int id) {
+        log.trace("got={}", id);
+
+        boolean success = false;
         try {
             if (txManager.doInTransaction(() -> accountRepository.deletAccount(id))) {
-                return true;
+                success = true;
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            log.warn("Account was not deleted!", e);
         }
-        return false;
+        log.trace("returning={}", success);
+        return success;
     }
 }
--- src/main/java/com/bank/service/CreditCardServiceImpl.java
+++ src/main/java/com/bank/service/CreditCardServiceImpl.java
@@ -1,14 +1,15 @@
 package com.bank.service;
 
-import com.bank.model.Client;
 import com.bank.model.CreditCard;
 import com.bank.repository.CreditCardRepositoryImpl;
 import com.bank.repository.txManager.TxManager;
 import com.bank.repository.txManager.TxManagerImpl;
+import lombok.extern.slf4j.Slf4j;
 
 import java.sql.SQLException;
 import java.util.List;
 
+@Slf4j
 public class CreditCardServiceImpl implements CreditCardsInterface {
 
     private CreditCardRepositoryImpl cardRepository;
@@ -22,54 +23,73 @@ public class CreditCardServiceImpl implements CreditCardsInterface {
 
     @Override
     public List<CreditCard> getAll(int accountId) {
+        log.trace("id={}", accountId);
+
         List<CreditCard> cards = null;
         try {
             cards = cardRepository.getAllCards(accountId);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            log.warn("id={}", accountId, e);
         }
+        log.trace("returning={}", cards);
+
         return cards;
     }
 
     @Override
     public CreditCard getById(int id) {
+        log.trace("id={}", id);
+
         CreditCard card = new CreditCard();
         try {
             card = cardRepository.getCardById(id);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            log.warn("id={}", id, e);
         }
+        log.trace("returning={}", card);
         return card;
     }
 
     @Override
     public CreditCard update(CreditCard card) {
+        log.trace("got={}", card);
+
         try {
             cardRepository.updateCard(card);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            log.warn("Card was not updated!", e);
         }
+        log.trace("returning={}", card);
+
         return card;
     }
 
     @Override
     public boolean delete(int cardId) {
+        log.trace("got={}", cardId);
+
+        boolean success = false;
         try {
-            if (txManager.doInTransaction(() -> cardRepository.deleteCard(cardId))) return true;
+            if (txManager.doInTransaction(() -> cardRepository.deleteCard(cardId))) success = true;
         } catch (Exception e) {
-            e.printStackTrace();
+            log.warn("Card was not deleted!", e);
         }
-        return false;
+        log.trace("returning={}", success);
+        return success;
     }
 
     @Override
     public CreditCard add(int accountId, CreditCard card) {
+        log.trace("got={}", card);
+
         CreditCard card1 = new CreditCard();
         try {
-             card1 = cardRepository.addCard(accountId, card);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+            card1 = cardRepository.addCard(accountId, card);
+        } catch (SQLException e) {
+            log.warn("Card was not created!", e);
         }
+        log.trace("returning={}", card1);
+
         return card1;
     }
 
