38794c95ca4191cb77e674c70dd887212cf6cae9
Sergey_N <mr.s.v.novikov@gmail.com>
2020-10-09 10:07:04 +0300

    Alpha 0.2

--- src/main/java/com/bank/service/AccountService.java
+++ src/main/java/com/bank/service/AccountService.java
@@ -2,15 +2,42 @@ package com.bank.service;
 
 import com.bank.model.Account;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface AccountService {
 
-    Account getById(int parentId, int entityId);
+    /**
+     * Get CreditCard by Id.
+     *
+     * @param clientId
+     * @param accountId
+     * @return CreditCard.
+     */
+    Account getById(int clientId, int accountId);
 
+    /**
+     * Find and return all client's accounts by Id.
+     *
+     * @param id
+     * @return List<Account>
+     */
     List<Account> getAll(int id);
 
+    /**
+     * Persist Account.
+     *
+     * @param account
+     * @return Account.
+     */
     Account save(Account account);
 
-    boolean delete(int cId, int aId);
+    /**
+     * Delete Account by Id.
+     *
+     * @param clientId
+     * @param accountId
+     * @return true if success.
+     */
+    boolean delete(int clientId, int accountId);
 }
--- src/main/java/com/bank/service/CreditCardService.java
+++ src/main/java/com/bank/service/CreditCardService.java
@@ -6,12 +6,41 @@ import java.util.List;
 
 public interface CreditCardService {
 
+    /**
+     * Get CreditCard by Id.
+     *
+     * @param clientId
+     * @param accountId
+     * @param cardId
+     * @return CreditCard.
+     */
     CreditCard getById(int clientId, int accountId, int cardId);
 
+    /**
+     * Returns all card for this accountId.
+     *
+     * @param clientId
+     * @param accountId
+     * @return List<CreditCard>
+     */
     List<CreditCard> getAll(int clientId, int accountId);
 
-    CreditCard save(CreditCard card);
+    /**
+     * Persist CreditCard.
+     *
+     * @param creditCard
+     * @return creditCard
+     */
+    CreditCard save(CreditCard creditCard);
 
+    /**
+     * Delete Card by Id.
+     *
+     * @param clientId
+     * @param accountId
+     * @param cardId
+     * @return true if success.
+     */
     boolean delete(int clientId, int accountId, int cardId);
 
 }
